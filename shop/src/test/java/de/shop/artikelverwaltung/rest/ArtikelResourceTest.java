6bc7163158507b005228416d8704036ad593a912
Julian Hartweg <haju1012 (at) hs-karlsruhe.de>
2013-11-21 17:36:34 +0100

    ArtikelResourceTest funktioniert jetzt

--- shop/src/test/java/de/shop/artikelverwaltung/rest/ArtikelResourceTest.java
+++ shop/src/test/java/de/shop/artikelverwaltung/rest/ArtikelResourceTest.java
@@ -1,21 +1,12 @@
 package de.shop.artikelverwaltung.rest;
 
-import static de.shop.util.TestConstants.ARTIKEL_URI;
 import static de.shop.util.TestConstants.ARTIKEL_ID_PATH_PARAM;
 import static de.shop.util.TestConstants.ARTIKEL_ID_URI;
-import static de.shop.util.TestConstants.BESTELLUNGEN_URI;
-import static de.shop.util.TestConstants.KUNDEN_ID_URI;
-import static de.shop.util.TestConstants.PASSWORD;
-import static de.shop.util.TestConstants.USERNAME;
-import static java.net.HttpURLConnection.HTTP_CREATED;
 import static java.net.HttpURLConnection.HTTP_OK;
-import static javax.ws.rs.client.Entity.json;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.lang.invoke.MethodHandles;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.logging.Logger;
 
 import javax.ws.rs.core.Response;
@@ -26,14 +17,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import de.shop.artikelverwaltung.domain.Artikel;
-import de.shop.bestellverwaltung.domain.Bestellung;
 import de.shop.util.AbstractResourceTest;
 
 @RunWith(Arquillian.class)
 public class ArtikelResourceTest extends AbstractResourceTest {
 	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 	
-	private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(400);
+	private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(300);
 	
 	@Test
 	@InSequence(1)
